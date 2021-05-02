cd90fcf773c994deefedca3ec012bd141061c38b
amitkumardube <amitkumardube@gmail.com>
2021-05-02 22:31:42 +0300

    Adjusted the workflow

--- test/fixtures/secrets/resource.tf
+++ test/fixtures/secrets/resource.tf
@@ -3,7 +3,7 @@
 // Calling the base rool module code to ensure that we are close the real infra 
 
 module "bigquery_svc_account" {
-  source = "../../../../code/big_query/"
+  source = "../../../code/big_query/"
   bigquery_dataset_name = var.bigquery_dataset_name
   account_name = var.account_name
   bucket_name = var.bucket_name
